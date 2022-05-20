5396e8f1c7dfcef31c91aef044533c843b4f8e60
ericogn <92058697+ericogn@users.noreply.github.com>
2022-05-20 23:16:27 +0300

    miscellanous

--- proiectLaII/proiect-ui/src/app/app-routing.module.ts
+++ proiectLaII/proiect-ui/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
+import { MainPageComponent } from './main-page/main-page.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path: 'login', component:LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'about', component:AboutComponent}
+  {path: 'about', component:AboutComponent},
+  {path: '', component:MainPageComponent}
 ];
 
 @NgModule({
