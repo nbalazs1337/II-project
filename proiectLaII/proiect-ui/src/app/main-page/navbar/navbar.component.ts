b8ab9e3c6b8ad923aa3a6c0b9126a5a20a57feab
ericogn <92058697+ericogn@users.noreply.github.com>
2022-05-20 23:00:23 +0300

    added routing between login and register form

--- proiectLaII/proiect-ui/src/app/main-page/navbar/navbar.component.ts
+++ proiectLaII/proiect-ui/src/app/main-page/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router ) { }
 
   ngOnInit(): void {
+   
+  }
+
+  register(){
+    this.router.navigateByUrl('/register');
+  }
+
+  login(){
+    this.router.navigateByUrl('/login')
   }
 
 }
