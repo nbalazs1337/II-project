505ca6ba8f9b7c99b297a3686019e98bbeb41ada
ericogn <92058697+ericogn@users.noreply.github.com>
2022-05-24 22:54:05 +0300

    final commit

--- proiectLaII/proiect-ui/src/app/main-page/brands-list/brands-list.component.ts
+++ proiectLaII/proiect-ui/src/app/main-page/brands-list/brands-list.component.ts
@@ -13,13 +13,22 @@ export class BrandsListComponent implements OnInit {
 
   @Input() brandsList:Brand[] = [  ];
   @Input() products:Product[] = [  ];
-  brandsControl:FormGroup
+  brandsControl:FormGroup;
+  selectedValue: any;
   constructor(private userService: UserService) {
     this.brandsControl = new FormGroup(
       {
         "name": new FormControl(null)
       }
     )
+   }
+
+   onChange(event:any) {
+     console.log('\n event \n', event);
+     console.log(this.products);
+     this.userService.filterProducts(event).subscribe(l => this.userService.prods = l);
+    console.log(event);
+    console.log(this.userService.prods)
    }
   
 
---
+++ proiectLaII/proiectLaII/Test/Test.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using NUnit.Framework;
+using proiectLaII.Context;
+using proiectLaII.Models;
+using proiectLaII.Repository;
+
+namespace proiectLaII.Test
+{
+    public class Test
+    {
+        private StoreContext context;
+        private User user;
+        [Test]
+        public void testUser()
+        {
+            UserRepository repo = new UserRepository(context);
+            Mock<IUserRepository> usershandler = new Mock<IUserRepository>();
+
+            Assert.AreEqual(usershandler.Object, repo);
+
+        }
+    }
+}
